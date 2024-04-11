e21cdc6917fef0f8c72f8d383aec75d0d67223a1
paulbud61 <paulbud61@gmail.com>
2024-04-11 23:17:19 +0300

    Update : Responsive Design and New Functionalities

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,48 @@
-import React from "react";
-import { useState } from "react";
-import styles from "./form.module.css";
+import React, { useState } from 'react';
+import styles from './form.module.css';
 
 export default function Form({ tasks, setTasks }) {
-  // const [task, setTask] = useState("");
-  const [task, setTask] = useState({ name: "", done: false });
+  const [task, setTask] = useState({ name: '', category: '', done: false });
+  const categories = [
+    'Personal',
+    'Work',
+    'Study',
+    'Fitness',
+    'Household',
+    'Errands',
+    'Social',
+    'Health',
+    'Finance',
+  ];
 
   function handleSubmit(e) {
     e.preventDefault();
     setTasks([...tasks, task]);
-    setTask({ name: "", done: false });
+    setTask({ name: '', category: '', done: false });
   }
+
   return (
     <form className={styles.taskform} onSubmit={handleSubmit}>
       <div className={styles.inputContainer}>
         <input
           className={styles.modernInput}
-          onChange={(e) => setTask({ name: e.target.value, done: false })}
+          onChange={e => setTask({ ...task, name: e.target.value })}
           value={task.name}
           type="text"
           placeholder="Enter task name"
         />
+        <select
+          className={styles.modernInput}
+          onChange={e => setTask({ ...task, category: e.target.value })}
+          value={task.category}
+        >
+          <option value="">Select category</option>
+          {categories.map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
         <button className={styles.modernButton} type="submit">
           Add
         </button>
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,39 +1,43 @@
-import styles from "./taskItem.module.css";
+import React from 'react';
+import styles from './taskItem.module.css';
 
 export default function TaskItem({ item, tasks, setTasks }) {
   function handleDelete(item) {
-    setTasks(tasks.filter((task) => task !== item));
+    setTasks(tasks.filter(task => task !== item));
   }
 
   function handleClick(name) {
     setTasks(
-      tasks.map((task) =>
+      tasks.map(task =>
         task.name === name ? { ...task, done: !task.done } : task
       )
     );
   }
 
   function handleEdit(item) {
-    const editedName = prompt("Enter the new name for the task:");
-    if (editedName !== null && editedName.trim() !== "") {
+    const editedName = prompt('Enter the new name for the task:');
+    if (editedName !== null && editedName.trim() !== '') {
       setTasks(
-        tasks.map((task) =>
+        tasks.map(task =>
           task === item ? { ...task, name: editedName } : task
         )
       );
     }
   }
 
-  const className = item.done ? styles.completed : "";
+  const className = item.done ? styles.completed : '';
 
   return (
     <div className={styles.item}>
-      <div className={styles.itemName}>
-        <span className={className} onClick={() => handleClick(item.name)}>
+      <div className={styles.itemContent}>
+        <span
+          className={`${styles.itemName} ${className}`}
+          onClick={() => handleClick(item.name)}
+        >
           {item.name}
         </span>
-
-        <span>
+        <span className={styles.category}>{item.category}</span>
+        <span className={styles.blueButton}>
           <button
             onClick={() => handleDelete(item)}
             className={styles.deleteButton}
@@ -48,7 +52,6 @@ export default function TaskItem({ item, tasks, setTasks }) {
           </button>
         </span>
       </div>
-
       <hr className={styles.line} />
     </div>
   );
