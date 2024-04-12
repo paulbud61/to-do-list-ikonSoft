8464f42e80ee1e2dec509a6effa7e5885418d90f
paulbud61 <paulbud61@gmail.com>
2024-04-12 15:36:13 +0300

    Changed The Text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,7 +39,7 @@ export default function Header() {
     <div className={styles.header}>
       <span>My Task List</span>
       <button className={styles.button} onClick={toggleModal}>
-        Click me for Info&Details
+        Click for Information & Details
       </button>
       {modalOpen && (
         <>
@@ -49,39 +49,42 @@ export default function Header() {
               <button className={styles.closeButton} onClick={handleCloseModal}>
                 x
               </button>
-              <h2>Info Point and Instructions</h2>
-              <p>This is a To-Do-List project I made for IkonSoft Company.</p>
+              <h2>Information and Instructions</h2>
+              <p>
+                This is a To-Do-List project developed for IkonSoft Company.
+              </p>
               <h3>How does the site work?</h3>
               <ul>
                 <li>
-                  Type in the tasks you have to do today and select a category
+                  Enter the tasks you need to accomplish today and select a
+                  category.
                 </li>
                 <li>
-                  If you wrote the wrong word or letter, you can edit the task
+                  If you make a mistake while typing, you can edit the task.
                 </li>
                 <li>
-                  When you are done with one task, just click/press on it and it
-                  will be cut through with a line
+                  Once you have completed a task, simply click or tap on it, and
+                  it will be crossed out with a line.
                 </li>
               </ul>
-              <h3>How I created the website and my experience so far</h3>
+              <h3>Development process and experience</h3>
               <ul>
-                <li>I created the site by using React + Vite</li>
-                <li>I first started working on Task.jsx</li>
+                <li>I developed the site using React + Vite.</li>
+                <li>I began by working on Task.jsx.</li>
                 <li>
-                  After creating the head, the form, the functionality with
-                  handle Functions and e.current.target, I split the code in
-                  Header, TaskItem, Task Name and Form components, to make it
-                  readable for other less experienced users
+                  After creating the header, the form, and implementing
+                  functionality with event handlers and e.current.target, I
+                  organized the code into Header, TaskItem, TaskName, and Form
+                  components to improve readability for less experienced users.
                 </li>
                 <li>
-                  For storing the data, I used LocalStorage (setItem and
-                  getItem). Probably in the near future I will work with Redux
-                  Persist, for personal reasons
+                  For data storage, I utilized LocalStorage (setItem and
+                  getItem). In the near future, I may explore Redux Persist for
+                  personal reasons.
                 </li>
                 <li>
-                  This is simply a project I worked on, on the last 5 days. Have
-                  a nice day and I hope it meets User's requirments!
+                  This project represents 4 days of work. I hope it meets the
+                  requirements of the users. Have a great day!
                 </li>
               </ul>
             </div>
